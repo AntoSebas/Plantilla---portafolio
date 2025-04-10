612566699b39bdf25d97dfd6991c37ff93a03077
AntoSebas <anthonygilces4@gmail.com>
2025-04-09 19:30:01 -0500

    Portafolio

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+console.log('soy el archivo java scrip y estoy listo');
+
+const sectionDom = document.getElementById("section-list-cards");
+const createdCard = document.createElement("div");
+createdCard.classList.add("relative", "flex", "flex-col", "text-gray-700", "bg-white", "shadow-md", "bg-clip-border", "rounded-xl", "w-96");
+createdCard.innerHTML = 
+
+console.log(sectionDom, createdCard);
